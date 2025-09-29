c5526a649122f0096c6536b489ba38473f6ef587
“Tarun <vaghelatarun90@gmail.com>
2025-09-29 16:41:41 +0530

    updated redis

--- backend/cache.js
+++ backend/cache.js
@@ -1,32 +1,11 @@
-// cache.js
 import Redis from "ioredis";
-import dotenv from 'dotenv';
-dotenv.config({path:"./.env"});
-let redis;
-try {
-redis = new Redis({
-    username: process.env.REDIS_USERNAME,
-    password: process.env.REDIS_PASSWORD,
-    socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
-    }
-});  
-} catch (error) {
-  console.log(error.message)
-  console.log("Error Connecting  Cloud Redis")
-  console.log("FallBack to  local redis server")
-  // Connect to Redis (default host: 127.0.0.1, port: 6379)
-  redis = new Redis();
-}
+import dotenv from "dotenv";
+dotenv.config();
 
+const redis = new Redis(process.env.REDIS_URL);
 
-
-
-
-// Test connection
 redis.on("connect", () => {
-  console.log("✅ Connected to Redis");
+  console.log("✅ Connected to Redis Cloud");
 });
 
 redis.on("error", (err) => {
